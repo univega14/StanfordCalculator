11345e6e61e65f1288842fdf1d0e136afc177f0a
Alexey Roslyakov <ros.alex79@gmail.com>
2016-05-09 01:11:38 +0300

    Initial Commit

---
+++ StanfordCalculator/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  StanfordCalculator
+//
+//  Created by Univega on 24.04.16.
+//  Copyright © 2016 Univega. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet private weak var display: UILabel!
+    
+     private var userIsInTheMiddleOfTyping = false
+    
+    private var brain = CalculatorBrain()
+    
+    @IBAction private func touchDigit(sender: UIButton) {
+            let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+        }
+       userIsInTheMiddleOfTyping = true
+      
+    }
+   
+    var savedProgram: CalculatorBrain.PropertyList?
+    
+    @IBAction func save() {
+        savedProgram = brain.program
+    }
+    
+    @IBAction func restore() {
+        if savedProgram != nil {
+            brain.program = savedProgram!
+            displayValue = brain.result
+        }
+    }
+    
+    
+    
+    
+    private var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+            
+        }
+    }
+    
+    @IBAction private func performOperation(sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        if let mathematicalSymbol = sender.currentTitle {
+            brain.performOperation(mathematicalSymbol)
+        }
+        displayValue = brain.result
+    }
+    
+}
+
